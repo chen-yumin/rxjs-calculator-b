278ce8538659d9178820f76943a03181b4090a2b
CharmySoft <charmysoft@gmail.com>
2016-11-22 20:03:43 +0000

    Calculator from worksheet 3

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -52,7 +52,56 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	console.log('Program is Running...');
+	var result = 0;
+	var operation = '';
+	var displayRefresh = false;
+
+	var calcTempResult = function calcTempResult() {
+	  var v = parseFloat(document.getElementsByTagName('input')[0].value);
+	  if (operation === '+') {
+	    result += v;
+	  } else if (operation === '-') {
+	    result -= v;
+	  } else if (operation === 'x' || operation === '*') {
+	    result *= v;
+	  } else if (operation === '÷' || operation === '/') {
+	    result /= v;
+	  } else {
+	    result = v;
+	  }
+	  document.getElementsByTagName('input')[0].value = result;
+	};
+
+	var processKey = function processKey(key) {
+	  if (/\d/.test(key) || key === '.') {
+	    // If the button is a number
+	    if (displayRefresh) {
+	      document.getElementsByTagName('input')[0].value = key;
+	      displayRefresh = false;
+	    } else {
+	      document.getElementsByTagName('input')[0].value += key;
+	    }
+	  } else if (key === 'C') {
+	    result = 0;
+	    operation = '';
+	    document.getElementsByTagName('input')[0].value = '0';
+	  } else {
+	    calcTempResult();
+	    operation = key;
+	    displayRefresh = true;
+	  }
+	};
+
+	var btns = document.getElementsByClassName("flex-item");
+	[].forEach.call(btns, function (btn) {
+	  btn.onclick = function () {
+	    processKey(btn.textContent);
+	  };
+	});
+
+	document.getElementsByTagName('body')[0].addEventListener("keypress", function (event) {
+	  processKey(event.key);
+	});
 
 /***/ },
 /* 1 */
